4b0e2337cabfb4fbec53bf56ad942e93fb256c89
juls-teacher <juliayurchyk@gmail.com>
2022-12-25 21:15:27 +0300

    Add classwork4

--- lessons_4/classwork4_05.py
+++ lessons_4/classwork4_05.py
@@ -1,2 +1,8 @@
 """Получить сумму кубов натуральных чисел от n до m используя цикл for, числа n и m вводятся с клавиатуры."""
 
+n= int(input("vvedi nachalnoe chislo"))
+m= int(input ("vvedi poslednee chislo "))
+sum = 0
+for i in range(n,m):
+    sum+=i**3
+print (sum)
