409871ff2a4f2deb72847562198cdd587b66f77d
juls-teacher <juliayurchyk@gmail.com>
2022-12-26 21:08:19 +0300

    Add

---
+++ lessons_5/classwork5_01.py
@@ -0,0 +1,11 @@
+"""Написать функцию, которая получает на вход имя и выводит строку вида: f"Hello, {name}". Создать список из 5 имен. Вызвать функцию для каждого элемента списка в цикле."""
+def print_name(name):
+    print(f"Hello, {name}")
+my_name=["sasha","olga","ula","ula"]
+for name in my_name:
+    print_name(name)
+
+
+
+
+
