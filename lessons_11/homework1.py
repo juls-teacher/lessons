881437ec85c0b898ab46f34429fbddd16f1e548e
juls-teacher <juliayurchyk@gmail.com>
2023-01-30 11:35:59 +0300

    Add

--- lessons_11/homework1.py
+++ lessons_11/homework1.py
@@ -1,7 +1,6 @@
 """Создать таблицу продуктов. Атрибуты продукта: id, название, цена, количество, комментарий. Реализовать
 следующие функции для продуктов: создание, чтение, обновление по id, удаление по id. """
 
-
 import sqlite3
 
 def create_product(name: str, price: int, number:int, comment:str):
@@ -16,8 +15,36 @@ def create_product(name: str, price: int, number:int, comment:str):
        )
        session.commit()
 
+def show_products():
+    with    sqlite3.connect("products.sqlite3") as session:
+        cursor = session.cursor()
+        cursor.execute(
+           """
+          SELECT *
+          FROM shop;
+          """
+        )
+    session.commit()
+    return cursor.fetchall()
+
+def delete_product_by_id(id :int):
+    with    sqlite3.connect("products.sqlite3") as session:
+        cursor = session.cursor()
+        cursor.execute(" DELETE FROM shop  WHERE id =? ; ", (id))
+    session.commit()
+    return cursor.fetchall()
 
 
 #if __name__== "main":
-create_product("Phone",1200,12,"mobile phone")
-create_product("Washmachine", 450,10, "None")
+create_product("Phone",1200,12,"Philips")
+create_product("Wash machine", 450,10, "Bosch")
+create_product("refrigerator", 1100,35, "Samsung")
+create_product("coffee machine", 120,80, "Tassimo ")
+
+all = show_products()
+print('\n'.join(map(str,all)))
+
+del_pr = delete_product_by_id(124)
+print(del_pr)
+
+
