6fa2552d372be7eddb0cfca0aa637cdca14897f1
juls-teacher <juliayurchyk@gmail.com>
2023-02-01 14:12:10 +0300

    Add create, update , sort

--- lessons_12/classwork12_01.py
+++ lessons_12/classwork12_01.py
@@ -42,11 +42,11 @@ if __name__ == "__main__":
         profile = Profile(phone='+375251111115', age='30', user_id=user.id)
         session.add(profile)
 
-        address = Address(city='Minsk', address='ul. Sovetskaya 23', user_id=user.id)
+        address = Address(city='Minsk', address='ul. Sovetskaya 23', user_id=275)
         session.add(address)
-        address = Address(city='Brest', address='ul. Pervomayskaya 14', user_id=user.id)
+        address = Address(city='Brest', address='ul. Pervomayskaya 14', user_id=276)
         session.add(address)
-        address = Address(city='Minsk', address='ul. Minskaya 4', user_id=user.id)
+        address = Address(city='Minsk', address='ul. Minskaya 4', user_id=277)
         session.add(address)
         address = Address(city='Mogilev', address='ul. Gurskogo 7 ', user_id=user.id)
         session.add(address)
@@ -55,7 +55,26 @@ if __name__ == "__main__":
         session.commit()
 
 
+#Создать функции для добавления нового и обновления существующего адреса пользователя.
+    def update_address():
+        address = Address(city='New York', address='test', user_id=277)
+        session.add(address)
+        session.commit()
+        session.query(Address).filter_by(city='Gomel').update({"city": "Gomel !!!!!"})
+        session.add(address)
+        session.commit()
+
+#Создать функцию для поиска всех пользователей с определенным возрастом.
+    def search_age():
+
+        profile = session.query(Profile).filter(Profile.age >= 26)
+        for row in profile:
+            print(f'{row.phone}.{row.age}.{row.id}')
+        session.commit()
+
 
 
 
     add_user()
+    update_address()
+    search_age()
