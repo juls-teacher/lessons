352d7b60942a331a4c1ff48e1742da1c4c30476d
juls-teacher <juliayurchyk@gmail.com>
2023-02-06 11:34:33 +0300

    error problem with table products

--- lessons_12/classwork12_01.py
+++ lessons_12/classwork12_01.py
@@ -1,18 +1,23 @@
-from pathlib import Path
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+#from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import create_database, database_exists
+from lessons_12.models import Base, User ,Profile, Address, Product, Purchase
 
-from lessons_12.models import Base, User ,Profile, Address
-
-DB_PATH = Path(__file__).resolve().parent / "my_database.sqlite3"
+DB_USER = "juls"
+DB_PASSWORD = "juls"
+DB_NAME = "juls"
 DB_ECHO = True
 
-
 if __name__ == "__main__":
-    engine = create_engine(f"sqlite:////{DB_PATH}", echo=DB_ECHO)
+    engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost/{DB_NAME}", echo=True,)
     if not database_exists(engine.url):
         create_database(engine.url)
+
 
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
@@ -30,6 +35,7 @@ if __name__ == "__main__":
         session.add(user)
         user = User(email="test5@test.com", password="password5")
         session.add(user)
+        session.commit()
 
         profile = Profile(phone='+375251111111', age='25', user_id=user.id)
         session.add(profile)
@@ -73,8 +79,116 @@ if __name__ == "__main__":
         session.commit()
 
 
+    def create_user():
+        user = User(email=input('Введите электронную почту '), password=input('Введите пароль '))
+        session.add(user)
+        session.commit()
+
+        address = Address(user_id=user.id, city=input('Введите город '), address=input('Введите адрес '))
+        session.add(address)
+
+        profile = Profile(user_id=user.id, phone=input('Введите номер телефона '), age=input('Введите возраст '))
+        session.add(profile)
+
+        session.commit()
 
 
-    add_user()
-    update_address()
-    search_age()
+    def update_address2():
+        session.query(User).filter(User.email == input('Введите почту ')).first()
+        address = session.query(Address).filter(Address.address == input('Введите старый адрес ')).first()
+        address.address = input('Введите новый адрес ')
+        session.add(address)
+        session.commit()
+
+
+    def add_address():
+        address = Address(user_id=input('Введите ID '), city=input('Введите город '), address=input('Введите адрес '))
+        session.add(address)
+        session.commit()
+
+
+    def find_user():
+        result = session.query(Profile).filter(Profile.age == int(input('Введите возраст пользователя ')))
+        for user in result:
+            logger.info(user)
+
+
+    def create_product():
+        product = Product(name=input('Введите название продукта '), price=float(input('Введите цену ')),
+                          count=int(input('Введите количество ')), comment=input('Введите комментарий '))
+        session.add(product)
+        session.commit()
+
+
+    def read_product():
+        result = session.query(Product).filter(Product.name == input('Введите название продукта '))
+        for product in result:
+            logger.info(product)
+
+
+    def update_product():
+        product = session.query(Product).filter(Product.id == int(input('Введите ID '))).first()
+        product.name = input('Введите название продукта ')
+        product.price = float(input('Введите цену '))
+        product.count = int(input('Введите количество '))
+        product.comment = input('Введите комментарий ')
+        session.add(product)
+        session.commit()
+
+
+    def delete_product():
+        product = session.query(Product).filter(Product.id == int(input('Введите ID '))).first()
+        session.delete(product)
+        session.commit()
+
+
+    def buy_product():
+        # product_id = session.query(Product.id).filter(Product.name == input('Введите название продукта' ))
+        # user_id = session.query(User.id).filter(User.email == input('Введите почту'))
+        purchase = Purchase(user_id=4, product_id=4, count='1')
+        session.add(purchase)
+        session.commit()
+
+
+    #
+    # add_user()
+    # update_address()
+    # search_age()
+
+    # create_user()
+    # update_address2()
+    # add_address()
+    # find_user()
+
+    create_product()
+    #read_product()
+    # update_product()
+    # delete_product()
+    #buy_product()
+
+
+
+
+
+    # user = User(email="test@test.com", password="password")
+    # session.add(user)
+    #
+    # session.commit()
+    #
+    # address = Address(user_id=user.id, city="Minsk", address="Test")
+    # session.add(address)
+    #
+    # profile = Profile(user_id=user.id, phone="+375292999999", age=20)
+    # session.add(profile)
+    #
+    # user = session.query(User).filter(User.email == "test@test.com").first()
+    # user.password = "new_password"
+    # session.add(user)
+    # session.commit()
+    #
+    # result = session.query(Profile).filter(Profile.age >= 15)
+    # for x in result:
+    #     print(x)
+    #     print(x.user)
+    #
+
