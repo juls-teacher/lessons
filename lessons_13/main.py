ca81055623eb9c0cf52d0357ab8e1fbcdd69bf30
juls-teacher <juliayurchyk@gmail.com>
2023-02-08 12:34:22 +0300

    Add homework

--- lessons_13/main.py
+++ lessons_13/main.py
@@ -3,9 +3,9 @@ import logging
 from sqlalchemy import create_engine
 
 from sqlalchemy_utils import create_database, database_exists
-from lessons_12.models import Base, User ,Profile, Address, Product, Purchase
+from lessons_12.models import Base, User, Profile, Address, Product, Purchase
 from lessons_13.utilys import create_tables
-from lessons_13.utilys import create_user
+from lessons_13.utilys import create_user, create_product,find_user, add_purchase, search_purchases_by_user, delete_product,update_product
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -25,11 +25,23 @@ if __name__ == "__main__":
     menu= """
         1 .Создать пользователя
         2. Найти пользователя
-        3. EXIT
+        3. Добавить товар
+        4. Добавить покупку
+        5.Ввывод всех товаров, купленных определенным пользователем (only one!!)
+        6 .Удалить продукт
+        7. Обновить продукт
+        
+        
+        
+        
+        
+        
+        8. EXIT
         """
 
     while True:
         logger.info(menu)
+        print()
         choice = input("выберите действие:  ")
 
 
@@ -38,6 +50,45 @@ if __name__ == "__main__":
             password = input('Введите пароль ')
             user = create_user(session, email, password)
             logger.info(f'пользователь  #{user.id} создан')
-        elif choice == "3":
-            exit()
 
+        if choice =="2":
+            email = input('Введите электронную почту ')
+            user = find_user(session,email)
+            logger.info(f'пользователь  # {user.id} найден')
+
+        if choice =="3":
+            name=input('Введите название продукта ')
+            price=float(input('Введите цену '))
+            count=int(input('Введите количество '))
+            comment=input('Введите комментарий ')
+            product= create_product(session, name, price, count, comment)
+            logger.info(f'продукт  #{product.id} создан')
+
+        if choice =="4":
+            user_id = int(input('Введите id пользователя: '))
+            product_id = int(input('Введите id продукта: '))
+            count = int(input('Введите количество продукта: '))
+            purchase = add_purchase (session,user_id,product_id,count)
+            logger.info(f'покупка   {purchase.id} создана ')
+
+        if choice =="5":
+            email = input('Введите email пользователя ')
+            purchase = search_purchases_by_user(session, email)
+            logger.info (f'{purchase.user.email}, {purchase.product.name}, количество {purchase.count}')
+
+        if choice == "6":
+            product_id = input('Введите ID продукта ')
+            product = delete_product(session, product_id)
+            logger.info(f'Продукт #{product_id} удален ')
+
+        if choice =="7":
+            product_id = int(input('Введите id продукта: '))
+            name = input('Введите название продукта: ')
+            price = float(input('Введите цену продукта: '))
+            count = int(input('Введите количество продукта'))
+            comment = input('Введите комментарий')
+            update_product(session, product_id, name, price, count, comment)
+            logger.info(f'Продукт #{product_id} update ')
+
+        elif choice == "8":
+            exit()
--- lessons_13/utilys.py
+++ lessons_13/utilys.py
@@ -1,12 +1,14 @@
 from sqlalchemy.orm import sessionmaker
 
 from lessons_13.models2 import Base
-from lessons_13.models2 import User
+from lessons_13.models2 import User, Product, Purchase,Profile
+
 
 def create_tables(engine):
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     return Session()
+
 
 def create_user(session, email, password):
     user = User(email=email, password=password)
@@ -14,7 +16,54 @@ def create_user(session, email, password):
     session.commit()
     return user
 
-def find_user(session,email):
+
+def create_product(session, name, price, count, comment):
+    product = Product(name=name, price=price, count=count, comment=comment)
+    session.add(product)
+    session.commit()
+    return product
+
+
+def find_user(session, email):
     return session.query(User).filter(User.email == email).first()
+
+def add_purchase(session, user_id, product_id,count):
+    purchase = Purchase(user_id=user_id, product_id = product_id, count=count)
+    session.add(purchase)
+    session.commit()
+    return purchase
+
+
+def delete_product(session, product_id):
+    session.query(Product).filter_by(id=product_id).delete()
+    session.commit()
+
+def update_product(session,product_id, name, price,count, comment):
+     session.query(Product).filter_by(id=product_id).update({"name": f"{name}", "price": f"{price}","count": f"{count}", "comment": f"{comment}"})
+     session.commit()
+
+
+def search_purchases_by_user(session, email):
+    user_id = session.query(User.id).filter(User.email == email)
+    result = session.query(Purchase).filter(Purchase.user_id == user_id)
+    for it in result:
+        return it
+
+
+
+def search_product_by_user(session,user_id):
+
+    session.query(Purchase).filter(Purchase.user_id == user_id)
+    user_id = session.query(User.id).filter(User.email == email)
+    result = session.query(Purchase).filter(Purchase.user_id == user_id).first()
+    return result
+
+
+
+
+# Добавить функцию вывода всех товаров, купленных определенным пользователем.
+# Добавить функцию вывода всех пользователей, которые покупали определенный товар.
+# Добавить фильтрацию пользователей по купленным товарам.
+
 
 
